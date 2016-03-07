8a6d13e73d65d97e12219153eb9038ad6ff4fb48
Phillip Demro <phillip.demro@hotmail.com>
2016-03-07 11:34:45 -0500

    updates for spoHelpers

--- spoHelpers.js
+++ spoHelpers.js
@@ -1,57 +1,8 @@
-function spoHelpers() {
-    var setMasterPage = function(masterPageUrl) {
-        var clientContext = SP.ClientContext.get_current();
-        var website = clientContext.get_web();
-
-        clientContext.load(website);
-        clientContext.executeQueryAsync(function() { 
-            //do nothing
-        }, function() {console.log("fail")});
-                
-        // var masterPageUrl = "/_catalogs/masterpage/oslo.master";
-        
-        website.set_masterUrl(masterPageUrl);
-        website.update();
-        clientContext.load(website);
-        
-        clientContext.executeQueryAsync(function() { 
-            console.log("New MasterUrl for " + website.get_title() +": " + website.get_masterUrl())
-            
-            window.website = website;
-        }, function(sender, args) {console.log("fail:"); console.log(args.get_message())});
-    }
-
-    var setAlternateCssUrl = function(alternateCssUrl) {
-        var clientContext = SP.ClientContext.get_current();
-        var website = clientContext.get_web();
-
-        clientContext.load(website);
-        clientContext.executeQueryAsync(function() {
-            //do nothing
-        }, function() {console.log("fail")});
-                
-        //var alternateCssUrl = "/_catalogs/masterpage/oslo.master";
-        
-        website.set_alternateCssUrl(alternateCssUrl);
-        website.update();
-        clientContext.load(website);
-        
-        clientContext.executeQueryAsync(function() { 
-            console.log("New AlternateCssUrl for " + website.get_title() +": " + website.get_alternateCssUrl())
-            
-            window.website = website;
-        }, function(sender, args) {console.log("fail:"); console.log(args.get_message())});
-    }
-    
-    return {
-        setAlternateCssUrl : setAlternateCssUrl,
-        setMasterPage : setMasterPage
-    }
-}
+var SP = SP || {};
 
 var onload = function() { 
     
-    window.spoHelpers = spoHelpers();
+    window.spoHelpers = new SpoHelpers();
     
 	console.log("SharePoint Online Debug Window Helpers Loaded");
 };
@@ -62,4 +13,64 @@ var onload = function() {
 
 SP.SOD.executeFunc('sp.js', 'SP.ClientContext', onload);
 
+
+
+
+/* Spo Helpers
+ * This class is a collection of static methods to help QoL when prototyping in O365 through the debug window
+ */
+function SpoHelpers() {
+    
+    var _ctx = {};
+    var _web = {};
+    this.web = {}
+    this.ctx = {}
+    
+    var init = function(obj) { 
+        _ctx = SP.ClientContext.get_current();
+        _web = _ctx.get_web();
+        _ctx.load(_web);
+        _ctx.executeQueryAsync(function() { 
+            obj.web = _web;
+            obj.ctx = _ctx;
+            console.log("Initialized spoHelpers")
+        }, function() {console.log("fail")});
+    }
+    init(this);
+    
+    this.initialize = function() {
+        init();
+    };    
+
+    
+    this.setMasterPage = function(masterPageUrl) {        
+        _web.set_masterUrl(masterPageUrl);
+        _web.update();
+        _ctx.load(_web);
+        
+        _ctx.executeQueryAsync(function() { 
+            
+            console.log("New MasterUrl for " + _web.get_title() +": " + _web.get_masterUrl())
+            
+        }, function(sender, args) {console.log("fail:"); console.log(args.get_message())});
+    }
+
+    this.setAlternateCssUrl = function(alternateCssUrl) {
+        _web.set_alternateCssUrl(alternateCssUrl);
+        _web.update();
+        _ctx.load(_web);
+        
+        _ctx.executeQueryAsync(function() { 
+            
+            console.log("New AlternateCssUrl for " + _web.get_title() +": " + _web.get_alternateCssUrl())
+            
+        }, function(sender, args) {console.log("fail:"); console.log(args.get_message())});
+    }
+    
+    // return {
+    //     setAlternateCssUrl : setAlternateCssUrl,
+    //     setMasterPage : setMasterPage,
+    //     setJavascriptViaCustomAction : setJavascriptViaCustomAction
+    // }
+}
  
